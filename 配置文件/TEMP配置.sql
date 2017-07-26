9e53033bfd4a676429f3132eb64ddcb0a21edf61
Thomas Liu <liujiachengzhenniu@qq.com>
2017-07-26 14:01:00 +0800

    windows temp

---
+++ 配置文件/TEMP配置.sql
@@ -0,0 +1,9 @@
+/*
+
+用户的   TEMP   ： %USERPROFILE%\AppData\Local\Temp
+        TMP    ： %USERPROFILE%\AppData\Local\Temp
+        
+系统的   TEMP   ： %SystemRoot%\TEMP
+        TMP    ： %SystemRoot%\TEMP
+              
+*/
