45c86e3dc08d1715b61b671e83f56031c438fb7c
Thomas Liu <liujiachengzhenniu@qq.com>
2017-08-03 16:52:43 +0800

    拣货情况查询 新加两个查询  拣货零头查询  和 新的查询脚本

--- 开发/拣货计划新查询语句.sql
+++ 开发/拣货计划新查询语句.sql
@@ -21,10 +21,50 @@ select sum(tpd.fqtystr) num,
    and tp.stat <> 5210
    and tpd.fqtystr not like '%+%'
    and tpd.fpicker <> 0
-   and tpd.fpicker||'-' <> '-'
+   and tpd.fpicker || '-' <> '-'
    and to_char(tp.flstupdtime, 'yyyyMMdd') like '201707%'
  group by tpd.fpicker;
 
+select tpd.num,
+       tpd.fqtystr,
+       tpd.fpicker,
+       (select employee.name from employee where employee.gid = tpd.fpicker) fname
+  from tpickupdtl tpd
+  left join tpickup tp on tp.num = tpd.num
+ where tpd.fowner = '100081'
+   and tp.stat <> 5210
+   and tpd.fqtystr like '%+%'
+   and tpd.fpicker <> 0
+   and tpd.fpicker || '-' <> '-'
+   and to_char(tp.flstupdtime, 'yyyyMMdd') like '201707%'
 
- 
- 
+
+select tc.FSRCNUM      来源单号,
+       TC.FJOBID       配货作业号,
+       tc.num          配货通知单号,
+       tp.num          拣货单号,
+       tpd.line        行号,
+       ts.fname        门店名称,
+       t.fcode         商品代码,
+       t.fname         商品名称,
+       tpd.fqpcstr     商品规格,
+       tpd.fqtystr     箱数,
+       tpd.fqpc        包装规格,
+       tpd.fqty        件数,
+       tpd.fpalletcode 托盘条码,
+       tpd.FCHESTNUM   周转箱条码,
+       E.NAME          拣货员,
+       TP.FLSTUPDTIME  时间
+  from TPICKUPDTL tpd
+  left join tpickup tp on tp.num = tpd.num
+  left join talcntc tc on tpd.falcntc = tc.num
+  left join tstore ts on TP.FSTORE = TS.FCODE
+  left join tarticle t ON tpd.farticle = t.fgid
+  left join employee e on tpd.fpicker = E.GID
+ where tpd.fowner = '100081'
+   and tp.stat <> 5210
+   and tpd.fqtystr not like '%+%'
+   and tpd.fpicker <> 0
+   and tpd.fpicker || '-' <> '-'
+   and to_char(tp.flstupdtime, 'yyyyMMdd') like '201707%'
+ order by TP.FLSTUPDTIME;
