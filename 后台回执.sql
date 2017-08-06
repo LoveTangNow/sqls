4496148b04044e660014d1e2cd2452735398640c
Thomas Liu <liujiachengzhenniu@qq.com>
2017-08-06 11:18:02 +0800

    后太回执加入上海的 移库拉取方法

--- 后台回执.sql
+++ 后台回执.sql
@@ -5,25 +5,35 @@ select * from tmoveinvdtl t where t.num in ('00011706000080');
 
 update tmoveinvdtl td
    set td.frealqtystr = td.fqtystr, td.frealqty = td.fqty
- where td.num in ('00011706000080');
-
+ where td.num in ('00011706000080'); --替换为相应的单号
 update tmoveinv t
    set t.stat = 100, t.flstupdtime = sysdate
- where t.num in ('00011706000080');
+ where t.num in ('00011706000080'); --替换为相应的单号
+ commit;
+ --以上数据执行完成之后过15分钟查看有没有回执成功
+
+select * from ll_move_ok l where l.type = 1 and l.num = '00011706000080'--替换为相应的单号
+--有数据表明回执成功，没有数据表明没有回执成功
 
 
-SELECT distinct t.num outer_bill_no, t.flstupdtime bill_date
-    from TMOVEINV t, wms_taobao_bill_history h
-  where t.stat = 100
-    and h.type = 3
-    and t.flstupdtime >= h.update_time
-    and t.flstupdtime > sysdate - 7
-    and t.ftype <> '门店退货'
-  ORDER BY t.num
-
-
-
-
+select *
+  from (select distinct tm.num outer_bill_no, tm.flstupdtime bill_date
+          from tmoveinv tm
+          left join tmoveinvdtl tmd on tm.num = tmd.num
+         where ((tmd.ffrompsn like '2%' and tmd.fdestpsn like '9%') or
+               (tmd.ffrompsn like '91%' and tmd.fdestpsn like '92%') or
+               (tmd.ffrompsn like '9%' and tmd.fdestpsn like '2%') or
+               (tmd.ffrompsn like '92%' and tmd.fdestpsn like '91%'))
+           and tm.num not in
+               (select l.num
+                  from ll_move_ok l
+                 where l.type = 1
+                   and l.time >= to_date('20170713', 'yyyymmdd'))
+           and tm.flstupdtime >= to_date('20170713', 'yyyymmdd')
+           and tm.ftype <> '门店退货'
+           and tm.stat = 100
+         order by tm.flstupdtime)
+ WHERE rownum <= 1
 
 --盘盈单后台回执
 update tincinv t
