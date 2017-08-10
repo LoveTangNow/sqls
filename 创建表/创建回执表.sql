4c61e75c2c7e233e1dbee3d225c54b95d69a17fc
Thomas Liu <liujiachengzhenniu@qq.com>
2017-08-10 14:48:42 +0800

    回执表更新 加入描述

--- 创建表/创建回执表.sql
+++ 创建表/创建回执表.sql
@@ -9,7 +9,6 @@ create table LRETURNBILL
   FMESSAGE VARCHAR2(200)          --描述
 )
 tablespace HDAPP
-
   pctfree 10
   initrans 1
   maxtrans 255
@@ -21,4 +20,15 @@ tablespace HDAPP
   );
 -- Add comments to the table 
 comment on table LRETURNBILL
-  is '成功移库单表';
+  is '回执表';
+-- Add comments to the columns 
+comment on column LRETURNBILL.FNUM
+  is '单号';
+comment on column LRETURNBILL.FTIME
+  is '回执时间';
+comment on column LRETURNBILL.FTYPE
+  is '单据种类：CK,RK,GT,MT,PY,PK,YK';
+comment on column LRETURNBILL.FSTATE
+  is '状态：0初始状态，1已经对账';
+comment on column LRETURNBILL.FMESSAGE
+  is '描述';
