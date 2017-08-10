61e80dd50b243220e7ad14fe9f98a5dd7c62d7c8
Thomas Liu <liujiachengzhenniu@qq.com>
2017-08-10 14:48:16 +0800

    服务更新脚本 加入深圳的更新部分

--- 脚本/update.cmd
+++ 脚本/update.cmd
@@ -1,9 +1,19 @@
 net stop SJRF
+
 xcopy C:\myws2017\tango\tango-web\target\tango-web-1.0-SNAPSHOT\* D:\Java\apache-tomcat-7.0.26-SJRF\webapps\tango-web\* /E /Y /C /F /I
 xcopy D:\Java\apache-tomcat-7.0.26-SJRF\webapps_bak\tango-web.properties D:\Java\apache-tomcat-7.0.26-SJRF\webapps\tango-web\WEB-INF\classes\tango-web.properties  /E /Y /C /F /I
+
 net start SJRF
 
 net stop SJRF-BJ
-xcopy C:\myws2017\tango\tango-web\target\tango-web-1.0-SNAPSHOT\* D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps\tango-web\* /E /Y /C /F /I
-xcopy D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps_bak\tango-web.properties D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps\tango-web\WEB-INF\classes\tango-web.properties  /E /Y /C /F /I
+
+xcopy C:\myws2017\tango\tango-web\target\tango-web-1.0-SNAPSHOT\* D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps\tango-web-bj\* /E /Y /C /F /I
+xcopy D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps_bak\tango-web-bj.properties D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps\tango-web-bj\WEB-INF\classes\tango-web.properties  /E /Y /C /F /I
+xcopy D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps_bak\log4j-bj.properties D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps\tango-web-bj\WEB-INF\classes\log4j.properties  /E /Y /C /F /I
+
+
+xcopy C:\myws2017\tango\tango-web\target\tango-web-1.0-SNAPSHOT\* D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps\tango-web-sz\* /E /Y /C /F /I
+xcopy D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps_bak\tango-web-sz.properties D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps\tango-web-sz\WEB-INF\classes\tango-web.properties  /E /Y /C /F /I
+xcopy D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps_bak\log4j-sz.properties D:\Java\apache-tomcat-7.0.26-SJRF-BJ\webapps\tango-web-bj\WEB-INF\classes\log4j.properties  /E /Y /C /F /I
+
 net start SJRF-BJ
